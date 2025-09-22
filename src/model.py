993157fcc17aec73fa0cc447b4facb4b5fdeb917
RaresPopa22 <poparares1992@gmail.com>
2025-09-22 21:32:53 +0300

    redesign cnn model

--- src/model.py
+++ src/model.py
@@ -4,6 +4,7 @@ from keras.src.applications.resnet import ResNet50
 from keras.src.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.utils import compute_class_weight
 from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.initializers import glorot_uniform
 
 
 def build_cnn_model(config):
@@ -19,33 +20,87 @@ def build_cnn_model(config):
     x = tf.keras.layers.Activation('relu')(x)
     x = tf.keras.layers.MaxPool2D(pool_size=3, strides=2)(x)
 
+    x = convolutional_block(x, f=3, filters=[64, 64, 256], s=1)
+    x = identity_block(x, f=3, filters=[64, 64, 256])
+    x = identity_block(x, f=3, filters=[64, 64, 256])
+
+    x = convolutional_block(x, f=3, filters=[128, 128, 512], s=2)
+
+    x = identity_block(x, f=3, filters=[128, 128, 512])
+    x = identity_block(x, f=3, filters=[128, 128, 512])
+    x = identity_block(x, f=3, filters=[128, 128, 512])
+
+    x = convolutional_block(x, f=3, filters=[256, 256, 1024], s=2)
+
+    x = identity_block(x, f=3, filters=[256, 256, 1024])
+    x = identity_block(x, f=3, filters=[256, 256, 1024])
+    x = identity_block(x, f=3, filters=[256, 256, 1024])
+    x = identity_block(x, f=3, filters=[256, 256, 1024])
+    x = identity_block(x, f=3, filters=[256, 256, 1024])
+
+    x = convolutional_block(x, f=3, filters=[512, 512, 2048], s=2)
+
+    x = identity_block(x, f=3, filters=[512, 512, 2048])
+    x = identity_block(x, f=3, filters=[512, 512, 2048])
+
+    f = tf.keras.layers.GlobalAveragePooling2D()(x)
+    outputs = tf.keras.layers.Dense(units=1, activation='sigmoid', kernel_initializer=glorot_uniform(0))(f)
+
+    model = tf.keras.Model(inputs=input_img, outputs=outputs)
+
+    return model
+
+
+def convolutional_block(x, f, filters, s=2, initializer=glorot_uniform):
+    f1, f2, f3 = filters
     x_shortcut = x
 
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding='same', strides=1)(x)
+    x = tf.keras.layers.Conv2D(
+        filters=f1, kernel_size=1, padding='valid', strides=s, kernel_initializer=initializer(0))(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
 
-    x = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding='same', strides=1)(x)
+    x = tf.keras.layers.Conv2D(
+        filters=f2, kernel_size=f, padding='same', strides=1, kernel_initializer=initializer(0))(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
 
-    x = tf.keras.layers.Conv2D(filters=512, kernel_size=3, padding='same', strides=1)(x)
+    x = tf.keras.layers.Conv2D(
+        filters=f3, kernel_size=1, padding='valid', strides=1, kernel_initializer=initializer(0))(x)
     x = tf.keras.layers.BatchNormalization()(x)
 
-    x_shortcut = tf.keras.layers.Conv2D(filters=512, kernel_size=1, padding='same', strides=1)(x_shortcut)
+    x_shortcut = tf.keras.layers.Conv2D(
+        filters=f3, kernel_size=1, padding='same', strides=s, kernel_initializer=initializer(0))(x_shortcut)
     x_shortcut = tf.keras.layers.BatchNormalization()(x_shortcut)
 
     x = tf.keras.layers.Add()([x, x_shortcut])
     x = tf.keras.layers.Activation('relu')(x)
 
-    f = tf.keras.layers.GlobalAveragePooling2D()(x)
-    dense = tf.keras.layers.Dense(units=128, activation='relu')(f)
-    dropout = tf.keras.layers.Dropout(0.2)(dense)
-    outputs = tf.keras.layers.Dense(units=1, activation='sigmoid')(dropout)
+    return x
 
-    model = tf.keras.Model(inputs=input_img, outputs=outputs)
 
-    return model
+def identity_block(x, f, filters, initializer=glorot_uniform):
+    f1, f2, f3 = filters
+    x_shortcut = x
+
+    x = tf.keras.layers.Conv2D(
+        filters=f1, kernel_size=1, padding='valid', strides=1, kernel_initializer=initializer(0))(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    x = tf.keras.layers.Conv2D(
+        filters=f2, kernel_size=f, padding='same', strides=1, kernel_initializer=initializer(0))(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    x = tf.keras.layers.Conv2D(
+        filters=f3, kernel_size=1, padding='valid', strides=1, kernel_initializer=initializer(0))(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+
+    x = tf.keras.layers.Add()([x, x_shortcut])
+    x = tf.keras.layers.Activation('relu')(x)
+
+    return x
 
 
 def build_resnet_50(config):
